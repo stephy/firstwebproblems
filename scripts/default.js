fede6ed05d8dff086ea8241b226a2c8f2ce2d68b
Stephy <alves@stephanimoroni.com>
2013-07-31 21:18:41 -0700

    adding files

---
+++ scripts/default.js
@@ -0,0 +1,26 @@
+/*
+	First Web Problems
+	Javascript 
+	by Stephani Alves & Kevin Deng
+*/
+
+console.log('loaded');
+
+<script>
+function allowDrop(ev)
+{
+ev.preventDefault();
+}
+
+function drag(ev)
+{
+ev.dataTransfer.setData("Text",ev.target.id);
+}
+
+function drop(ev)
+{
+ev.preventDefault();
+var data=ev.dataTransfer.getData("Text");
+ev.target.appendChild(document.getElementById(data));
+}
+</script>
