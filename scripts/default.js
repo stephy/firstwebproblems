f6779b0f2c3dba6e73a5ec4d03cf96d6874dbed3
Stephy <alves@stephanimoroni.com>
2013-08-07 21:31:00 -0700

    adding design

--- scripts/default.js
+++ scripts/default.js
@@ -4,4 +4,29 @@
 	by Stephani Alves & Kevin Deng
 */
 
+function resizeSection(){
+	if($(window).height() > 900){
+		$('.section').css('height',  $(window).height());	
+	}else{
+		$('.section').css('height',  '900px');
+	}
+	
+}
 
+function animateLogos(){
+	/*
+	for(var i=0; i< 100; i++){
+
+		var lil_logo = 
+		$('#anime').append()
+	}
+	*/
+	
+}
+
+$(window).resize(function(){
+	resizeSection();
+});
+
+resizeSection();
+animateLogos();
